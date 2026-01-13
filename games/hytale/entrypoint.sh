e71926b972ee86c635592935e4c519abd64eea90
zzahkaboom24 <github@zzahkaboom24.de>
2026-01-13 19:35:04 +0100

    Update entrypoint.sh

--- games/hytale/entrypoint.sh
+++ games/hytale/entrypoint.sh
@@ -5,15 +5,15 @@ cd /home/container
 
 # If HYTALE_SERVER_SESSION_TOKEN isn't set, assume the user will log in themselves, rather than a host's GSP
 if [[ -z "$HYTALE_SERVER_SESSION_TOKEN" ]]; then
-	./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
-
+	if [ "$(uname -m)" = "aarch64" ]; then
+    	qemu-x86_64-static ./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
+	else
+    	./hytale-downloader/hytale-downloader-linux -patchline "$HYTALE_PATCHLINE" -download-path HytaleServer.zip
+	fi
+	
 	unzip -o HytaleServer.zip -d .
 
-	rm -f HytaleServer.zip
-elif [[ -f "HytaleServer.zip" ]]; then
-	unzip -o HytaleServer.zip -d .
-elif [[ -f "HytaleMount/HytaleServer.zip" ]]; then
-	unzip -o HytaleMount/HytaleServer.zip -d .
+	rm HytaleServer.zip
 fi
 
 /java.sh $@
